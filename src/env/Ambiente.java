411402573e30f8036b783d2d4e7ab8200267d7a6
SérgIA <sergia.voip@gmail.com>
2026-03-22 23:30:25 -0300

    Update Ambiente.java

--- src/env/Ambiente.java
+++ src/env/Ambiente.java
@@ -1,1 +1,10 @@
 
+@ARTIFACT
+public class Ambiente extends Artifact {
+
+    void init() {
+        defineObsProperty("sujeira", 5);
+        defineObsProperty("sujeira", 12);
+        defineObsProperty("sujeira", 20);
+    }
+}
